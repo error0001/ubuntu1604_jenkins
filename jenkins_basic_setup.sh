53630e8984fff9b8428c838d1ec369c92b4905e0
Andrew Churyumov <34455557+error0001@users.noreply.github.com>
2019-11-21 14:59:54 +0300

    Update jenkins_basic_setup.sh

--- jenkins_basic_setup.sh
+++ jenkins_basic_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# добавим ключ для репозитория
+# install jenkins
 echo "Add key in the rep of ubuntu, and waiting the message OK"
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo "Add file sources.list"
@@ -8,15 +8,17 @@ echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources
 sudo apt-get update
 echo "Install Jenkins"
 sudo apt-get install jenkins
+# install java
 echo "install java for start jenkins"
-#
-#
+sudo apt update
+sudo apt install openjdk-8-jdk
+# continue setup jenkins
+echo "Auto starting Jenkins"
+sudo systemctl status jenkins
+# ufw
 echo "Setting the ufw. Port 8080 will be enabled"
 sudo ufw allow 8080
 sudo ufw status
-echo "Start Jenkins"
-# sudo systemctl start jenkins
-sudo systemctl status jenkins
 echo "If Jenkins is ok, see loaded will be enabled."
 echo "Letsgo start jenkins across systemctl"
 echo "Done. Go to the link, ip_address_or_domain_name:8080 and set up more"
